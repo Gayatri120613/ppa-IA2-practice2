3a53ad90c0330b5adeddde307e97d804cacca4b6
Gayatri120613 <94883116+Gayatri120613@users.noreply.github.com>
2022-02-08 09:38:42 +0530

    Update p1original.c

--- p1original.c
+++ p1original.c
@@ -12,7 +12,7 @@ void find_area(float base, float height, float *area)
 }
 void output(float base, float height, float area)
 {
-  printf("the are of triangle with base %f and height %f is %f",base,height,area);
+  printf("the area of triangle with base %f and height %f is %f",base,height,area);
 }
 int main()
 {
